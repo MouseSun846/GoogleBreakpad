f82de678894e7ce8b078fffba31467425d744450
wufanguitar <wu.fanguitar@163.com>
2019-01-15 22:10:52 +0800

    解决模拟器下无法生成dump信息（编译时需切换到NDK 16b）

--- breakpad/.externalNativeBuild/cmake/debug/arm64-v8a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ breakpad/.externalNativeBuild/cmake/debug/arm64-v8a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Windows")
 set(CMAKE_HOST_SYSTEM_VERSION "10.0.17134")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("D:/03_AndroidSDK/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+include("D:/03_AndroidSDK/ndk/android-ndk-r16b/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
